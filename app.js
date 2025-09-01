92f0b4cba67e66ac7dcda21e7148a9d214895815
Billy <42595835+wrammm@users.noreply.github.com>
2025-08-31 21:50:21 -0500

    fix: fixed port bug

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
